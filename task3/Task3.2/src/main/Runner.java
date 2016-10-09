3a13e6201f419d641aba6968b9198e18f545fb6f
Poina97 <polina-grodno97@mail.ru>
2016-10-09 17:51:18 +0300

    Add task 3.2 version 1, variant 4

---
+++ task3/Task3.2/src/main/Runner.java
@@ -0,0 +1,10 @@
+package main;
+
+public class Runner {
+	private static RandomManager manager = new RandomManager();
+	public static void main(String[] args) {
+		manager.printNumberAndSum();
+
+	}
+
+}
